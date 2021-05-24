b151a20360f062e5bdd7d9722f749fdb49a0e123
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-05-24 16:55:01 -0300

    Creates classes to store the data of the application

--- React_begginer/ceep/src/data/ArrayNotes.js
+++ React_begginer/ceep/src/data/ArrayNotes.js
@@ -1,6 +1,7 @@
 export default class ArrayNotes {
     constructor() {
         this.notes = [];
+        this._subscribers = [];
     }
 
     createNote(title, text, category) {
@@ -10,6 +11,16 @@ export default class ArrayNotes {
 
     deleteNote(index) {
         this.notes.splice(index, 1);
+    }
+
+    subscribe(func) {
+        this._subscribers.push(func);
+    }
+
+    notify() {
+        this._subscribers.forEach(func => { //para cada inscrito, forEach estou passando essa função anônima, que é a função que recebe como parâmetro uma função a executa
+            func(this.categories);
+        }); 
     }
 }
 
--- React_begginer/ceep/src/data/Categories.js
+++ React_begginer/ceep/src/data/Categories.js
@@ -1,9 +1,22 @@
 export default class Categories {
     constructor() {
         this.categories = [];
+        this._subscribers = [];
+    }
+
+    subscribe(func) {
+        this._subscribers.push(func);
+    }
+
+    notify() {
+        this._subscribers.forEach(func => { //para cada inscrito, forEach estou passando essa função anônima, que é a função que recebe como parâmetro uma função a executa
+            func(this.categories);
+        }); 
     }
     
     addCategory(newCategory) {
         this.categories.push(newCategory);
-      }
+    }
 }
+
+//A categoria precisa notificar o form toda vez que uma mudança acontecer. E o form muda quando for notificado. O form observa as mudanças da categoria
