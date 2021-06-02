2f6125ca3b1cfb67c32891c4bbcc8e6d6f964d93
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-06-02 17:56:38 -0300

    Implementes React Router lib

--- React_Router/petshop/src/App.jsx
+++ React_Router/petshop/src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./assets/css/base/base.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 
 function App() {
-  const Router = () => {
-    const location = window.location.pathname;
-    if (location === "/sobre") {
-      return <Sobre />;
-    } else {
-      return <Home />;
-    }
-  };
-  return <>{Router()}</>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
