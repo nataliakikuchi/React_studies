e2a13e64980eca6400a0bc79d826ac304116520d
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-06-02 16:21:33 -0300

    Creates first route

--- React_Router/petshop/src/App.jsx
+++ React_Router/petshop/src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './assets/css/base/base.css'
+import React from "react";
+import "./assets/css/base/base.css";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
 
 function App() {
-  return (
-  <>
-
-  </>
-  )
+  const Router = () => {
+    const location = window.location.pathname;
+    if (location === "/sobre") {
+      return <Sobre />;
+    } else {
+      return <Home />;
+    }
+  };
+  return <>{Router()}</>;
 }
 
-export default App
+export default App;
