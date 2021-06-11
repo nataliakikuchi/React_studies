1f76058f6ef8ee427ae5a925562909bbceda7e8a
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-06-11 15:59:11 -0300

    Implements api to bring data to the components

---
+++ React_Router/petshop/src/api/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://localhost:5000",
+});
+
+export const search = async (url, setData) => {
+  const response = await api.get(url);
+  setData(response.data);
+};
